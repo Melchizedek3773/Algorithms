701550a1584ce4b7ca11b11811d27a0e29c42ffd
Melchizedek <joker78hender@gmail.com>
2024-10-15 21:06:59 +0700

    ReverseArray
    You wrote size and elements array from 0 to size. In console u will see your array in reverse.

---
+++ ReverseArray/ReverseArray.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    int count = 0;
+    std::cin >> count;
+    int* arr = new int[count];
+    for (int i = 0; i < count; i++)
+        std::cin >> arr[i];
+    for (int i = count - 1; i >= 0; i--)
+        std::cout << arr[i] << " ";
+    delete[] arr;
+    return 0;
+}
